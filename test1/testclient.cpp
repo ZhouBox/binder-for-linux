8e972df8249a451c2918867e63bba5389c041586
zhouxiang <zhouxiangbetter@163.com>
2019-03-25 18:56:36 +0800

    add log

--- test1/testclient.cpp
+++ test1/testclient.cpp
@@ -7,13 +7,16 @@
 int main(int argc, char* argv[])
 {
 
+    std::cout << "testserviceclient" << std::endl;
     sp<IServiceManager> sm = defaultServiceManager();
     sp<IBinder> binder = sm->getService(String16("testservice"));
     if (binder.get() == NULL) {
+        std::cout << "testserviceclient -1" << std::endl;
         return -1;
     }
     sp<Itestservice> ts = interface_cast<Itestservice>(binder);
-    if (ts.get() == 0) {
+    if (ts.get() == NULL) {
+        std::cout << "testserviceclient -2" << std::endl;
         return -2;
     }
     auto _str = ts->testfun(String16("binder"));
